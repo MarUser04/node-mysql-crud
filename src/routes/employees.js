2bd70f3bd5edcb50112d9e86420db40acde8055f
Marco Useche <usechermarcoj@gmail.com>
2020-02-11 11:33:50 -0400

    basic crud 1.0

---
+++ src/routes/employees.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+
+const mysqlConnection =  require('../database');
+
+router.get('/', (req, res) => {
+  mysqlConnection.query('SELECT * FROM employees', (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  })
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id], (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  const query = "INSERT INTO `employees`(`name`,`salary`) VALUES ('"+req.body.name+"','"+req.body.salary+"')";
+  mysqlConnection.query(query, [req.body.name, req.body.salary], (err, rows, fields) => {
+    if(!err) {
+      res.json({Status: 'Employeed Saved'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+router.put('/:id', (req, res) => {
+  //const query = "UPDATE `employees`(`name`,`salary`) VALUES ('"+req.body.name+"','"+req.body.salary+"') WHERE id = "+req.params.id+"";
+  const query = 'UPDATE `employees` SET `name`=?, `salary`=? WHERE id =?';
+  mysqlConnection.query(query, [req.body.name, req.body.salary, req.params.id], (err, rows, fields) => {
+    if(!err) {
+      res.json({Status: 'Employeed Updated'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  mysqlConnection.query('DELETE FROM employees WHERE id =?', [id], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Employee deleted'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+module.exports = router;
